07984f487a6b686cae745ce0dbc4b2fd9e36f27f
Raw <>
2024-08-18 23:52:21 -0400

    remove theme-ui dependency

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,5 +1,5 @@
-import type { Theme } from 'theme-ui'
 import prism from './prism'
+
 const colors = {
   darker: '#11111b',
   dark: '#181825',
@@ -35,7 +35,7 @@ const cssQueries = {
     '@supports (-webkit-backdrop-filter: none) or (backdrop-filter: none)'
 }
 
-const theme: Theme = {
+const theme = {
   breakpoints: [32, 48, 64, 96, 128].map(w => `${w}em`),
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   fontSizes: [12, 16, 20, 24, 32, 48, 64, 96, 128, 160, 192],
